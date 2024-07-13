2354afe3da3cc34470dba22c9096f65b56608947
Iboro2 <nkereuwememmanueliboro05@gmail.com>
2024-07-13 08:17:39 +0100

    solidity_20%....learning more

---
+++ compile.js
@@ -0,0 +1,11 @@
+// compile code will go here
+const path = require('path');
+const fs = require('fs');
+
+const solc = require('solc')
+
+const inboxPath =  path.resolve(__dirname, 'contracts', 'inbox.sol');
+
+const source = fs.readFileSync(inboxPath, 'utf8');
+
+solc.compile(source, 1)
---
+++ contracts/Inbox.sol
@@ -0,0 +1,34 @@
+pragma solidity ^0.4.17;
+// linter warnings (red underline) about pragma version can igonored!
+
+// contract code will go here
+
+
+
+
+contract Inbox {
+    string public message;
+
+    function Inbox(string initialMessage) public {
+        message = initialMessage;
+    }
+
+    function setMessage(string newMessage) public {
+        message = newMessage;
+    }
+
+    function getMessage() public view returns (string) {
+        return message;
+    }
+
+   // function getMessage() public view returns (string) {
+    //     return message;
+    // }
+
+    //     function doMath(uint a, uint b) {
+    //         a + b;
+    //         a - b;
+    //         a * b;
+    //         a === 0;
+    // }
+}
