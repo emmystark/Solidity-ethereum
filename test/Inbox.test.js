4cd1de59b9b8b2b5259615940a16eca153b96feb
Iboro2 <nkereuwememmanueliboro05@gmail.com>
2024-07-13 14:05:34 +0100

    complete_test_for_a_class

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -17,9 +17,21 @@ class Car {
   }
 }
 
-describe('Car Class', ()=> {
-    it('can park', ()=> {
-        const car = new Car();
-        assert.equal(car.park(), 'sto09pped');
+let car;
+
+beforeEach(()=> {
+    car = new Car();
+})
+
+
+describe("Car Class", () => {
+//   const car = new Car();
+    it("can park", () => {
+        assert.equal(car.park(), "stopped");
+    });
+
+    it("can drive", () => {
+        // const car = new Car();
+        assert.equal(car.drive(), "vroom");
     });
 });
