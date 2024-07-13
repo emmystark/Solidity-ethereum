5ad5245b9c0b791802956a1fcce2e7255e86e179
Iboro2 <nkereuwememmanueliboro05@gmail.com>
2024-07-13 13:52:50 +0100

    passed test with mocha

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,6 +1,25 @@
-const ganache = require('ganache');
-const { Web3 } = require('web3');
+const assert = require("assert");
+const ganache = require("ganache");
+const { Web3 } = require("web3");
+const web3 = new Web3(ganache.provider());
 
 // updated ganache and web3 imports added for convenience
 
 // contract test code will go here
+
+class Car {
+  park() {
+    return "stopped";
+  }
+
+  drive() {
+    return "vroom";
+  }
+}
+
+describe('Car Class', ()=> {
+    it('can park', ()=> {
+        const car = new Car();
+        assert.equal(car.park(), 'stopped');
+    });
+});
