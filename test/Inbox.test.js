1c4bd692d431699f4db92da82c53fb7cce55c19e
Iboro2 <nkereuwememmanueliboro05@gmail.com>
2024-07-14 17:01:07 +0100

    writing_first_contarct

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -2,36 +2,61 @@ const assert = require("assert");
 const ganache = require("ganache");
 const { Web3 } = require("web3");
 const web3 = new Web3(ganache.provider());
+const { interface, bytecode } = require('../compile')
 
 // updated ganache and web3 imports added for convenience
 
 // contract test code will go here
 
-class Car {
-  park() {
-    return "stopped";
-  }
+// class Car {
+//   park() {
+//     return "stopped";
+//   }
 
-  drive() {
-    return "vroom";
-  }
-}
+//   drive() {
+//     return "vroom";
+//   }
+// }
 
-let car;
+// let car;
 
-beforeEach(()=> {
-    car = new Car();
+// beforeEach(()=> {
+//     car = new Car();
+// })
+
+
+// describe("Car Class", () => {
+// //   const car = new Car();
+//     it("can park", () => {
+//         assert.equal(car.park(), "stopped");
+//     });
+
+//     it("can drive", () => {
+//         // const car = new Car();
+//         assert.equal(car.drive(), "vroom");
+//     });
+// });
+
+let accounts;
+
+let inbox
+
+
+beforeEach(async ()=> {
+    //Get a list of all accounts
+    accounts = await web3.eth.getAccounts()
+    // .then(fetchedAccounts => {
+        // console.log(fetchedAccounts)
+    // })
+
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({data: bytecode, arguments: ['Hi there!']})
+    .send({from: accounts[0], gas: '1000000'})
+
+    //Use one of those accounts to deploy the contract
 })
 
-
-describe("Car Class", () => {
-//   const car = new Car();
-    it("can park", () => {
-        assert.equal(car.park(), "stopped");
-    });
-
-    it("can drive", () => {
-        // const car = new Car();
-        assert.equal(car.drive(), "vroom");
-    });
-});
+describe('Inbox', ()=> {
+    it('deploys a contract', ()=> {})
+    console.log(inbox)
+})
